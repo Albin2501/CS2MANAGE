33e6e7b3beb102212ba35c465feb582dfa54d0c5
Albin Gashi <100842942+Albin2501@users.noreply.github.com>
2023-08-19 19:15:55 +0200

    Item - Frontend of summary adjusted

--- frontend/src/app/service/toastify/toastify.service.ts
+++ frontend/src/app/service/toastify/toastify.service.ts
@@ -9,20 +9,20 @@ export class ToastifyService {
   constructor() { }
 
   successToast(text: string): void {
-    this.toast(text, 'var(--cs2DarkGreen)');
+    this.toast(text, 'var(--cs2DarkGreen)', 2000);
   }
 
   errorToast(text: string): void {
-    this.toast(text, 'var(--cs2DarkRed)');
+    this.toast(text, 'var(--cs2DarkRed)', 5000);
   }
 
-  private toast(text: string, backgroundColor: string): void {
+  private toast(text: string, backgroundColor: string, durationTime: number): void {
     Toastify({
       text: text,
-      duration: 5000,
+      duration: durationTime,
       close: true,
       gravity: 'bottom',
-      position: 'right',
+      position: 'left',
       stopOnFocus: true,
       style: {
         background: backgroundColor
