eed953c367704827aa86a5370f4d003db530f287
Albin2501 <100842942+Albin2501@users.noreply.github.com>
2025-04-20 19:22:19 +0200

    Automate - Streamlined deployment

--- BUILD.cmd
+++
@@ -1,2 +0,0 @@
-start cmd.exe /c "cd backend && npm install"
-start cmd.exe /c "cd frontend && npm install"
--- RUN.cmd
+++ RUN.cmd
@@ -1,3 +1,5 @@
+cd backend && call npm install && cd ..
+cd frontend && call npm install && cd ..
 start cmd.exe /k "cd backend && npm start"
 start cmd.exe /k "cd frontend && ng serve"
-explorer "http://localhost:4200"
+start "" "http://localhost:4200"
